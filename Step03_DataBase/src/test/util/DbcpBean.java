dda5e02ab6332d050c13447c1ed0b6ed7d09482d
kimgura <oli999@naver.com>
2020-01-06 11:23:07 +0900

    Step03_DataBase 프로젝트 만들고 Oracle DB 연동할 준비를 하였습니다.

---
+++ Step03_DataBase/src/test/util/DbcpBean.java
@@ -0,0 +1,38 @@
+package test.util;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DbcpBean {
+	//필드
+	private Connection conn;
+	//생성자
+	public DbcpBean() {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+			System.out.println("Connection 얻어오기 성공!");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Connection 객체를 리턴해주는 메소드
+	public Connection getConn() {
+		return conn;
+	}
+}
+
+
+
+
+
+
+
+
+
